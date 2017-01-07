102b91c24a191f06a2593f52733d11f07d07a414
Dom-nik <dominik.maj@gmail.com>
2017-01-07 14:20:43 +0100

    Pierwsza wersja - bardzo prosta, z alertami

---
+++ 01.JS_return/script.js
@@ -0,0 +1,13 @@
+//Funkcja podnosząca liczbę do kwadratu
+function square(number) {
+  result = number * number;
+  return result;
+}
+
+number1 = 5;
+number2 = 27;
+number3 = -15;
+
+alert(square(number1));
+alert(square(number2));
+alert(square(number3));
