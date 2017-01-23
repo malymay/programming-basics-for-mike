796f3eab7edb7e2351b5eee0c39706981790b6c1
michalmaj90 <michalmaj90@gmail.com>
2017-01-23 21:07:26 +0100

    01.JS_return

--- 01.JS_return/script2.js
+++ 01.JS_return/script2.js
@@ -1,12 +1,15 @@
 // Tym razem zrobimy małą interakcję z użytkownikiem
-function square(number) {
+function sum(number) {
   return number * number;
 }
 
-var number = prompt("Podaj liczbę, którą chcesz podnieść do kwardatu: ")
-var message = "Wynik to: " + square(number);
+var number = prompt("Podaj parametry, które chcesz do siebie dodać: ")
+var message = "Wynik to: " + sum(number);
 alert(message);
 // Zmiennej number można użyć drugi raz
-number = prompt("Podaj liczbę, którą chcesz podnieść do kwardatu: ")
+numbers = prompt("Podaj parametry, które chcesz do siebie dodać: ")
 // Zmienna message nie jest konieczna, można wiadomość skleić od razu w wywołaniu funkcji alert
-alert("Wynik to: " + square(number));
+alert("Wynik to: " + sum(numbers));
+
+
+// Przy mnożeniu możliwe jest tylko podawanie parametrów liczbowych. Przy dodawaniu możliwe jest łączenie ze sobą stringów i liczb
